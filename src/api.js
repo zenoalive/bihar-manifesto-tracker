1feb33d593e30eee5db55a44aff2b0db379295d8
Zenoalive <ashushahi22229@gmail.com>
2026-02-23 10:40:36 +0530

    checking

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const API = axios.create({
 //   baseURL: "http://localhost:5000/api", // your backend
 // });
-
+console.log("API URL:", import.meta.env.VITE_API_URL);
 const API = axios.create({
   baseURL: `${process.env.VITE_API_URL}/api/promises`, // your backend
 });
