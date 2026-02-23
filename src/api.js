0d37e62ec772d0cc5640fb50f2afa6ba008aea52
Zenoalive <ashushahi22229@gmail.com>
2026-02-23 11:33:55 +0530

    baseurl

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://bihar-manifesto-tracker.onrender.com",
 });
 
 export default API;
